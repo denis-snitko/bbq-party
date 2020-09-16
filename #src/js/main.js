15393874d3a4b9239ad562eab995c0ec9353b87b
Denis Snitko <denissnitko@gmail.com>
2020-09-16 12:39:08 +0300

    Add 18 age checked

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,1 +1,10 @@
+const elMainBody = document.querySelector('.main-body');
+let age = confirm('Вам есть 18 лет?');
 
+
+if (age) {
+  elMainBody.setAttribute('style', 'display: flex');
+} else {
+  elMainBody.remove();
+  document.location.href = 'http://google.com';
+}
