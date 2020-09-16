71a27834097749971e54e3f013f912eeed9d22e2
Denis Snitko <denissnitko@gmail.com>
2020-09-16 14:45:23 +0300

    Add cookies

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,12 +1,36 @@
 const elMainBody = document.querySelector('.main-body');
 const elModalCheck = document.querySelector('.modal-check');
 
-elModalCheck.addEventListener('click', (event) => {
-  
+let ageValue;
+let ageTrue = getCookie('age');
+
+function getCookie(name) {
+  let cookie = " " + document.cookie;
+  let search = " " + name + "=";
+  let setStr = null;
+  let offset = 0;
+  let end = 0;
+  if ( cookie.length > 0 ) {
+    offset = cookie.indexOf(search);
+    if ( offset != -1 ) {
+      offset += search.length;
+      end = cookie.indexOf(";", offset)
+      if ( end == -1 ) {
+        end = cookie.length;
+      }
+      setStr = unescape(cookie.substring(offset, end));
+    }
+  }
+  return (setStr);
+}
+
+function showContent(event) {
   if ( event.target.classList.contains('modal-check__yes') ) {
     
     elModalCheck.remove();
     elMainBody.setAttribute('style', 'display: flex');
+    ageValue = true;
+    document.cookie = 'age' + '=' + ageValue;
     
   } else if ( event.target.classList.contains('modal-check__no') ) {
     
@@ -14,5 +38,14 @@ elModalCheck.addEventListener('click', (event) => {
     document.location.href = 'http://google.com';
     
   }
-  
-})
+}
+
+if ( !ageTrue ) {
+  elModalCheck.addEventListener('click', showContent)
+} else {
+  elModalCheck.remove();
+  elMainBody.setAttribute('style', 'display: flex');
+}
+
+
+
