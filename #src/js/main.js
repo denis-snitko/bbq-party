535b7b1d5b71e28aaee243b953216de3aeff2207
Denis Snitko <denissnitko@gmail.com>
2020-09-17 12:20:49 +0300

    Редизайн и валидация формы

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,7 +1,19 @@
 const elMainBody = document.querySelector('.main-body');
 const elModalCheck = document.querySelector('.modal-check');
+const elSendButton = document.querySelector('.send');
+const elModalWarning = document.querySelector('.modal-warning');
+const elModalWarningText = document.querySelector('.modal-warning__text');
+const promoValue = document.querySelector('input[name="promo"]');
+const checkConditions = document.getElementById('conditions');
+const checkYes = document.getElementById('yes');
+const checkNo = document.getElementById('no');
+const checkName = document.getElementById('name');
+const checkFam = document.getElementById('fam');
+const checkTel = document.getElementById('tel');
+
 
 let ageValue;
+let promo = 111;
 let ageTrue = getCookie('age');
 
 function getCookie(name) {
@@ -40,6 +52,19 @@ function showContent(event) {
   }
 }
 
+
+function closeWarning(event) {
+  if ( event.target.tagName === 'SPAN' ) {
+    elModalWarning.setAttribute('style', 'display: none')
+  }
+}
+
+function showWarning() {
+  elModalWarning.setAttribute('style', 'display: block')
+  elModalWarning.addEventListener('click', closeWarning)
+}
+
+
 if ( !ageTrue ) {
   elModalCheck.addEventListener('click', showContent)
 } else {
@@ -48,4 +73,45 @@ if ( !ageTrue ) {
 }
 
 
-
+elSendButton.addEventListener('click', (event) => {
+  
+  if ( promoValue.value.length > 0 && promoValue.value == promo ) {
+    elModalWarningText.textContent = 'Заявка принята!'
+    showWarning()
+    
+  } else {
+    event.preventDefault()
+    elModalWarningText.textContent = 'Промокод введен не верно!'
+    showWarning()
+  }
+  
+  if (checkYes.checked || checkNo.checked) {
+    console.log('checked')
+  } else {
+    elModalWarningText.textContent = 'Вы должны определиться идете ли на мероприятие!'
+    showWarning()
+  }
+  
+  if(checkConditions.checked){
+    console.log(checkConditions.checked)
+  } else {
+    elModalWarningText.textContent = 'Вы не согласились с условиями акции!'
+    showWarning()
+  }
+  
+  if (checkName.value.length == 0) {
+    elModalWarningText.textContent = 'Заполните все поля!'
+    showWarning()
+  }
+  
+  if (checkFam.value.length == 0) {
+    elModalWarningText.textContent = 'Заполните все поля!'
+    showWarning()
+  }
+  
+  if (checkTel.value.length == 0) {
+    elModalWarningText.textContent = 'Заполните все поля!'
+    showWarning()
+  }
+  
+})
